302da19d3f2d8418da5ea0d827430698503d9cfa
Igor Gerasimov <gerasimov.igor.98@icloud.com>
2024-12-26 17:35:32 +0300

    fix: chmod

--- bitrix_to_postgres/start-airflow.sh
+++ bitrix_to_postgres/start-airflow.sh
@@ -7,5 +7,15 @@ mkdir -p ./dags ./logs ./plugins
 export AIRFLOW_UID=$(id -u)
 export AIRFLOW_GID=0
 
+# Устанавливаем правильные права доступа
+chmod -R 777 ./dags ./logs ./plugins
+chown -R ${AIRFLOW_UID}:${AIRFLOW_GID} ./dags ./logs ./plugins
+
+# Проверяем права доступа
+echo "Проверка прав доступа:"
+ls -la ./dags
+ls -la ./logs
+ls -la ./plugins
+
 # Запускаем Airflow
 # docker-compose -f docker-compose-airflow.yml up -d 
